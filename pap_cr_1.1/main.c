07dd83223ba7adf19077b6e45af1a89e36f984e3
王益静 <wangyijing@wangyijingdeMacBook-Air.local>
2015-04-18 03:42:49 +0800

    Initial Commit

---
+++ pap_cr_1.1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  pap_cr_1.1
+//
+//  Created by 王益静 on 15/4/18.
+//  Copyright (c) 2015年 王益静. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
