40b4980eede1db72553f0e216bf30963d987b066
王益静 <wangyijing@wangyijingdeMacBook-Air.local>
2015-04-20 22:53:50 +0800

    init project config

---
+++ pap_cr_code_1.1/algorithm/algorithm.h
@@ -0,0 +1,15 @@
+#ifndef __ALGORITHM_H__
+#define __ALGORITHM_H__
+
+#include "hashtable.h"
+#include "io_cnf_txt.h"
+#include "io_file.h"
+#include "io_log.h"
+#include "io_time.h"
+#include "pkt.h"
+#include "queuel.h"
+#include "util.h"
+
+#endif
+
+
